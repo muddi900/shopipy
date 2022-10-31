52e601b8aa5e2f2bddc181d0f17fe0eb0098a4e1
Mudassir Chapra <muddi900@gmail.com>
2022-10-30 20:51:40 -0500

    added headers

--- shopify.py
+++ shopify.py
@@ -12,7 +12,14 @@ class Shopify:
 
         if admin_key is None:
             raise TypeError("Admin key can't be none")
-        self.__url = f"https://{store_slug}.myshopify.com"
+        self.__url = f"https://{store_slug}.myshopify.com/admin/api/2022-07/"
+        self.__headers = {"X-Shopify-Access-Token": admin_key}
 
+    async def get_orders_async(self, limit=50):
+        async with httpx.AsyncClient(headers=self.__headers) as client:
+            orders = await client.get(f"{self.__url}/orders?limit={limit}")
 
-    def 
+        return orders.json()
+
+    def get_orders(self, limit=50):
+        return asyncio.run(self.get_orders_async(limit))
