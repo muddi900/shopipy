6f44eec4c29c7eca17c3a9d620fa4da555d8120d
Mudassir Chapra <muddi900@gmail.com>
2023-01-05 12:43:49 +0500

    admin key is now a kwarg
    reafctored client for cleaner code
    
    updated generic function for external client
    
    fixed `get_users`

--- shopipy/shopify.py
+++ shopipy/shopify.py
@@ -2,13 +2,22 @@ import os
 import asyncio
 import httpx
 
+from enum import Enum
 from typing import Any
+
+
+class BulkAction(Enum):
+    GET = 1
+    CREATE = 2
+    EDIT = 3
+    DELETE = 4
 
 
 class Shopify:
     def __init__(
         self,
         store_slug: str,
+        *,
         admin_key=os.environ.get("SHOPIFY_ADMIN_KEY", None),
     ) -> None:
 
@@ -17,55 +26,66 @@ class Shopify:
         self.__url = f"https://{store_slug}.myshopify.com/admin/api/2022-07"
         self.__headers = {"X-Shopify-Access-Token": admin_key}
 
+    async def __get_client(self, **kwargs) -> httpx.AsyncClient:
+        return await httpx.AsyncClient(headers=self.__headers, **kwargs)
+
     async def __get_item(
         self,
         *,
         url_json_path: str,
-        async_client: httpx.AsyncClient,
         limit: int | None,
     ) -> httpx.Response:
         if limit > 250:
             raise AttributeError("The max limit is 250")
 
+        async_client = await self.__get_client()
+
         url = f"{self.__url}/{url_json_path}"
 
         if limit is not None or limit > 0:
             url = f"{url}?limit={limit}"
-        return await async_client.get(url)
+        resp = await async_client.get(url)
+        async_client.close()
+        return resp
 
     async def __create_items(
         self,
         *,
+        client: httpx.AsyncClient = None,
         url_json_path: str,
-        client: httpx.AsyncClient,
-        limit: int | None,
         data: dict[Any, Any],
     ) -> httpx.Response:
 
         url = f"{self.__url}/{url_json_path}"
+        if client is None:
+            client = await self.__get_client()
 
-        return client.post(url, json=data)
+        resp = client.post(url, json=data)
+        client.close()
+        return resp
 
     async def get_orders(self, limit=50, *, order_id: str | int = None):
-        async with httpx.AsyncClient(headers=self.__headers) as client:
-            orders = await self.__get_item(
-                url_json_path="orders.json",
-                async_client=client,
-                limit=limit,
-            )
+        if order_id is not None:
+            json_path = f"orders/{order_id}.json"
+        else:
+            json_path = "orders.json"
+            limit = None
+
+        orders = await self.__get_item(url_json_path=json_path, limit=limit)
 
         return orders.json()
 
     def get_orders_sync(self, limit=50):
         return asyncio.run(self.get_orders_async(limit))
 
-    async def get_products(self, limit=50):
-        async with httpx.AsyncClient(headers=self.__headers) as client:
-            products = await self.__get_item(
-                url_json_path="products.json",
-                async_client=client,
-                limit=limit,
-            )
+    async def get_products(self, limit=50, product_id: str | int = None):
+        if product_id is None:
+            json_path = "products.json"
+        else:
+            json_path = f"products/{product_id}.json"
+            limit = None
+
+        products = await self.__get_item(url_json_path=json_path, limit=limit)
 
         return products.json()
 
@@ -73,12 +93,11 @@ class Shopify:
         return asyncio.run(self.get_products(limit))
 
     async def create_product(self, data: dict[Any, Any]) -> dict:
-        async with httpx.AsyncClient() as client:
-            resp = await self.__create_items(
-                data=data,
-                client=client,
-                url_json_path="products.json",
-            )
+
+        resp = await self.__create_items(
+            data=data,
+            url_json_path="products.json",
+        )
 
         return resp.json()
 
@@ -86,10 +105,9 @@ class Shopify:
         return asyncio.run(self.create_product(data))
 
     async def get_users(self):
-        async with httpx.AsyncClient() as client:
-            return await self.__get_item(
-                async_client=client, url_json_path="users.json"
-            )
+        resp = await self.__get_item(url_json_path="users.json")
+
+        return resp.a
 
     def get_users_sync(self):
         return asyncio.run(self.get_users())
