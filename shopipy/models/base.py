f32ae057d67016c9e806b0fa11fb7cf2a38b3501
Mudassir Chapra <muddi900@gmail.com>
2023-01-23 13:32:50 +0500

    added docstring

--- shopipy/models/base.py
+++ shopipy/models/base.py
@@ -3,6 +3,10 @@ from abc import ABC
 
 
 class BaseModel(ABC):
+    """
+    This is the base class. Due to the verbose nature of Shopify's json objects, having simple dict parse for the objects would save a lot of time.
+    """
+
     def __init__(self, data: dict) -> None:
         self.__data = data
 
