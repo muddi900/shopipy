c0bb2979329f9d4f2581978698ab9f1acffb423d
Mudassir Chapra <muddi900@gmail.com>
2023-01-24 16:45:34 +0500

    changed to customer

--- shopipy/models/customer.py
+++ shopipy/models/customer.py
@@ -1,6 +1,6 @@
 from base import BaseModel
 
 
-class User(BaseModel):
+class Customer(BaseModel):
     def __init__(self, data: dict) -> None:
         super().__init__(data, "email_marketing_consent")
