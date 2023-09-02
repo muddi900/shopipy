f9e600961bd9aecb8f4a5e60ecb874b9990a9944
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2023-09-02 13:48:32 +0500

    fixed errors

--- shopipy/models/webhook.py
+++ shopipy/models/webhook.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Webhook:
-    address: str,
-    api_version:str,
-    created_at: str,
+    address: str
+    api_version: str
+    created_at: str
     fields: list[str]
-    format: str,
-    id: int,
-    metafield_namespaces: list[str],
-    private_metafield_namespaces: list[str],
-    topic: str,
+    format: str
+    id: int
+    metafield_namespaces: list[str]
+    private_metafield_namespaces: list[str]
+    topic: str
     updated_at: str
