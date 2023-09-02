694e51aaf7d9adfff595eeb456e19ec49e29dbcb
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2023-09-02 12:29:20 +0500

    dummy test

--- tests/order_tests.py
+++ tests/order_tests.py
@@ -1,7 +1,9 @@
 import vcr
 import httpx
+from shopipy import Shopify
 
 
 @vcr.use_cassette()
-def test_order_resp(resp) -> None:
-    
+def test_order_resp() -> None:
+    s = Shopify("url_slug")
+    pass
